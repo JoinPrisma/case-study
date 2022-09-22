99e5ca0167e3743bf468b7d873d1eb748ad20490
JavierM42 <javier@wyeworks.com>
2022-09-22 10:53:45 -0300

    add blog articles

--- components/LinksCarousel.tsx
+++ components/LinksCarousel.tsx
@@ -5,29 +5,32 @@ const LinksCarousel: FC = () => {
   return (
     <ul className="w-screen py-20 mb-20 [&>*]:align-top [&>*]:inline-block overflow-x-auto snap-x snap-mandatory lg:snap-proximity ml-[50%] -translate-x-1/2 whitespace-nowrap px-[calc(50vw-50%)] scrollbar-none">
       {[
-        {
-          heading: 'Try it out!',
-          text: null,
-          linkText: 'Go to playground',
-          linkUrl: 'https://text.joinprisma.com',
-        },
-        {
-          heading: 'How our collaborative editor works',
-          text: 'Cake liquorice bear claw chocolate macaroon jelly beans sweet sesame snaps. Gummies chocolate jelly-o cheesecake sugar plum soufflé pudding lollipop caramels.',
-          linkText: 'Read now',
-          linkUrl: '',
-        },
-        {
-          heading: 'How we solved performance issues',
-          text: 'Icing sesame snaps chocolate cake chocolate bar halvah lemon drops oat cake.',
-          linkText: 'Read now',
-          linkUrl: '',
-        },
+        // {
+        //   heading: 'Try our text editor out!',
+        //   text: null,
+        //   linkText: 'Go to playground',
+        //   linkUrl: 'https://text.joinprisma.com',
+        // },
         {
           heading: 'How our collaborative editor works',
-          text: 'Cake liquorice bear claw chocolate macaroon jelly beans sweet sesame snaps. Gummies chocolate jelly-o cheesecake sugar plum soufflé pudding lollipop caramels.',
+          text: 'A series of articles on inspired by how we built a collaborative conflict-free editor.',
+          linkText: 'Read part 1 now',
+          linkUrl:
+            'https://www.streaver.com/blog/posts/crdts-and-distributed-consistency-part-1-building-a-distributed-counter',
+        },
+        {
+          heading: 'How did we make canvas changes performant?',
+          text: 'Re-mounting videos without losing frames was made possible thanks to React Reverse Portals.',
           linkText: 'Read now',
-          linkUrl: '',
+          linkUrl:
+            'https://www.wyeworks.com/blog/2022/09/12/optimizing-expensive-react-component-mounts',
+        },
+        {
+          heading: 'Screenshot testing UI components',
+          text: "To make sure our components didn't break on unrelated changes, we added automated screenshots to our CI pipeline.",
+          linkText: 'Read now',
+          linkUrl:
+            'https://www.wyeworks.com/blog/2022/03/16/screenshot-testing-react-components-with-viteshot',
         },
       ].map((cardData, index) => (
         <li
